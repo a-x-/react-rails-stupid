ed24c356ee1585416eefe73ea4a88265e0f270d1
alex <alex@rocketguys.com>
2018-11-03 19:55:45 +0300

    1.5.4

--- lib/react/rails/version.rb
+++ lib/react/rails/version.rb
@@ -2,6 +2,6 @@ module React
   module Rails
     # If you change this, make sure to update VERSIONS.md
     # And the version hint in README.md, if needed
-    VERSION = '1.5.3'
+    VERSION = '1.5.4'
   end
 end
