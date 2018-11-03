f317fc9af162381df5368a85d48751bf89f6dee4
alex <alex@rocketguys.com>
2018-11-03 19:41:31 +0300

    1.5.3

--- lib/react/rails/version.rb
+++ lib/react/rails/version.rb
@@ -2,6 +2,6 @@ module React
   module Rails
     # If you change this, make sure to update VERSIONS.md
     # And the version hint in README.md, if needed
-    VERSION = '1.5.2'
+    VERSION = '1.5.3'
   end
 end
--- release.todo.md
+++ release.todo.md
@@ -1,4 +1,5 @@
 - bump the lib/react/rails/version.rb
+- git add lib/react/rails/version.rb;git commit -m '1.5.2'
 - git tag v1.5.2
 - git push && git push --tags
 - gem build react-rails-stupid.gemspec
